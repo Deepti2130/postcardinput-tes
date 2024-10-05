2add17233c022edbc23b2c00ff65de94427d380e
Deepti2130 <160584555+Deepti2130@users.noreply.github.com>
2024-10-05 17:21:36 +0530

    postcard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ipost } from './shared/model/posts';
+import { postArr } from './shared/const/posts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'postcard';
+  postData! : Array<Ipost>
+
+  ngOnInit(): void {
+      this.postData = postArr;
+  }
 }
